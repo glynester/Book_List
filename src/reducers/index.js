a454bd2f8b0a645b137fa318803f4a1961c4eb9b
Gee <glynester@hotmail.com>
2022-06-16 22:31:44 +0100

    Redux container added for BookList to display list of books.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
 
+// key was the piece of state or the name of the piece of state and the value was the reducer itself.
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    {title:"Javascript: The Good parts"},
+    {title:"The Selfish Gene"},
+    {title:"The Dark Tower"},
+    {title:"Eloquent Ruby"},
+  ]
+}
